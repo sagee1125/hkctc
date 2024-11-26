f20d1628594d599caa4929787def83af10a00291
sa1jing <sa1jing.chen@polyu.edu.hk>
2024-11-26 16:28:49 +0800

    email box style fix

--- src/components/EmailBox/EmailBox.tsx
+++ src/components/EmailBox/EmailBox.tsx
@@ -4,7 +4,7 @@ export const EmailBox: React.FC = () => {
   return (
     <>
       <div className="text-heading-l pb-[16px]">Enquires</div>
-      <div className="bg-[#EAEAE5] w-full p-4 flex flex-col gap-4">
+      <div className="bg-[#EAEAE5] w-full px-[24px] py-[22px] flex flex-col gap-[16px]">
         <div className="text-body-m">
           Contact Us to know more about Testing and Certificate.
         </div>
