505d4c9ae06ffe14fc6a3ef0a981c2279fa80af5
sagee <sagebuggo1125@gmail.com>
2024-12-01 15:12:05 +0800

    resolve effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,15 @@ module.exports = {
         'italic': 'italic',
       },
 
+      animation: {
+        'fade-in': 'fadeIn 1s ease-in-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
 
     },
   },
