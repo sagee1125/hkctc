b10b26bcca9bd802c54b655ba38bf9693800382c
sagee <sagebuggo1125@gmail.com>
2024-12-08 19:18:43 +0800

    api try 8

--- api/hello.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-echo json_encode(["message" => "Hello from PHP"]);
