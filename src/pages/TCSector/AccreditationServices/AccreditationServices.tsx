a3bc5610f6be68a01637e112efd1e35772610e2f
sa1jing <sa1jing.chen@polyu.edu.hk>
2025-03-19 13:23:47 +0800

    fix: CN - Profile of the Sector

--- src/pages/TCSector/AccreditationServices/AccreditationServices.tsx
+++ src/pages/TCSector/AccreditationServices/AccreditationServices.tsx
@@ -2,31 +2,66 @@ import React from "react";
 import { MediaTemplateWithDialog, SquareTitle } from "../../../components";
 import { accreditationService } from "../../ServiceUsers";
 import { MEDIA_TYPE } from "../../../const";
-
+import { Language, useSettings } from "../../../context";
+const multilingual = {
+  en: {
+    accreditation_services: "Accreditation Services",
+    whats_accreditation: "What is Accreditation?",
+    accreditation_def: {
+      accreditation_is: "Accreditation is the",
+      third_party: "third-party attestation",
+      related_to:
+        "related to a conformity assessment body conveying formal demonstration of its competence to carry out specific conformity assessment tasks. Laboratories, inspection bodies and certification bodies are common types of conformity assessment bodies.",
+    },
+    accreditation_is_open:
+      "Accreditation is open and voluntary in Hong Kong. It is currently provided by Hong Kong Accreditation Service (HKAS) under Innovation and Technology Commission in Hong Kong. HKAS operates three accreditation schemes:",
+    this_full_list:
+      "*This full list is for the purpose of searching specific conformity assessment activity and will only be updated weekly. For most up-to-date scope of accreditation, please visit",
+    HKAS: `HKAS's website`,
+  },
+  cn: {
+    accreditation_services: "Accreditation Services",
+    whats_accreditation: "What is Accreditation?",
+    accreditation_def: {
+      accreditation_is: "Accreditation is the",
+      third_party: "third-party attestation",
+      related_to:
+        "related to a conformity assessment body conveying formal demonstration of its competence to carry out specific conformity assessment tasks. Laboratories, inspection bodies and certification bodies are common types of conformity assessment bodies.",
+    },
+    accreditation_is_open:
+      "Accreditation is open and voluntary in Hong Kong. It is currently provided by Hong Kong Accreditation Service (HKAS) under Innovation and Technology Commission in Hong Kong. HKAS operates three accreditation schemes:",
+    this_full_list:
+      "*This full list is for the purpose of searching specific conformity assessment activity and will only be updated weekly. For most up-to-date scope of accreditation, please visit",
+    HKAS: `HKAS's website`,
+  },
+};
 export const AccreditationServices: React.FC = () => {
+  const { language } = useSettings();
+  const page_text =
+    language === Language.EN ? multilingual.en : multilingual.cn;
+  const {
+    accreditation_services,
+    whats_accreditation,
+    accreditation_def,
+    accreditation_is_open,
+    this_full_list,
+    HKAS,
+  } = page_text;
   return (
     <div className="w-full">
       <div className="flex-1">
-        <SquareTitle title={"Accreditation Services"} />
+        <SquareTitle title={accreditation_services} />
         <div className="bg-[#EEEEEA] my-[24px] py-[36px] px-[42px]">
-          <p className="text-heading-m">What is Accreditation?</p>
+          <p className="text-heading-m">{whats_accreditation}</p>
           <p className="text-body-m mt-[16px]">
-            Accreditation is the&nbsp;
+            {accreditation_def.accreditation_is}&nbsp;
             <span className="text-highlight-m text-newPrimary">
-              third-party attestation
+              {accreditation_def.third_party}
             </span>
-            &nbsp;related to a conformity assessment body conveying formal
-            demonstration of its competence to carry out specific conformity
-            assessment tasks. Laboratories, inspection bodies and certification
-            bodies are common types of conformity assessment bodies.
+            &nbsp;{accreditation_def.related_to}
           </p>
         </div>
-        <p className="text-body-m mt-[24px]">
-          Accreditation is open and voluntary in Hong Kong. It is currently
-          provided by Hong Kong Accreditation Service (HKAS) under Innovation
-          and Technology Commission in Hong Kong. HKAS operates three
-          accreditation schemes:
-        </p>
+        <p className="text-body-m mt-[24px]">{accreditation_is_open}</p>
 
         <div className="w-full">
           {accreditationService.map((item, index) => {
@@ -50,16 +85,14 @@ export const AccreditationServices: React.FC = () => {
         </div>
 
         <p className="text-italic-s mt-[24px]">
-          *This full list is for the purpose of searching specific conformity
-          assessment activity and will only be updated weekly. For most
-          up-to-date scope of accreditation, please visit&nbsp;
+          {this_full_list}&nbsp;
           <a
             href="https://www.itc.gov.hk/en/quality/hkas/conformity_assessment_bodies/index.html"
             target="_blank"
             rel="noopener noreferrer"
             className="underline text-[#00E]"
           >
-            HKAS's website
+            {HKAS}
           </a>
           .
         </p>
