7d4017057fdf1e172918039ac3ea7656db2787ec
sa1jing <sa1jing.chen@polyu.edu.hk>
2025-03-24 18:07:09 +0800

    fix: fix/relaxation - CN

--- src/pages/Support/AccommodationAndLand/Relaxation.tsx
+++ src/pages/Support/AccommodationAndLand/Relaxation.tsx
@@ -16,113 +16,8 @@ const multilingual = {
     note: `Note 1: It refers to uses permitted under the land use zoning of the sites concerned on the relevant Outline Zoning Plans as “Column 1” uses.`,
     details_of_relaxation: {
       title: `Details of the Relaxation of Waiver Application Arrangement`,
-      para_1: `As the Development Bureau announced on 1 February 2019, the Government would permit five non-industrial uses in premises within IBs on a time-limited basis. The prerequisite is that such uses are always permitted under the relevant statutory plan, and do not involve direct provision of services or goods attracting visiting members of the public. Under the arrangement, an owner of part(s) of an existing IB does not need to apply for a temporary waiver from LandsD, and does not need to pay a waiver fee and an administrative fee which would otherwise be payable, for putting the relevant IB premises to such permitted non-industrial uses during the effective period. The effective period of the arrangement was initially set for five years, followed by an extension to 31 January 2025 announced on 26 January 2024, and a further extension to the end of 2027 announced in the 2024 Policy Address.`,
-      para_2: `"Research, Design and Development Centre" is one of the five permitted uses. According to the Definition of Terms published by the Town Planning Board, testing laboratories are covered by “Laboratory, Inspection and Testing Centre” which is subsumed under the use of “Research, Design and Development Centre”. As such, testing laboratories in industrial premises in compliance with the above prerequisites can benefit from the relaxation of waiver application arrangement. In determining whether the use of a premise has involved non-compliant uses outside the scope of the permission, LandsD will seek advice from the Innovation and Technology Commission and the decision of which shall be final.`,
-      para_3: `For any processed/processing temporary waiver application submitted in accordance with Practice Note No. 1/2016 issued by LandsD, no administrative fee will be refunded.`,
-      please_refer: `Please refer to the announcement of Development Bureau on 1 February 2019 and the press release of the extension on 26 January 2024, and the 2024 Policy Address for further details:`,
-    },
-    enquiries: `Enquiries`,
-    enquiries_about: `Enquiries about how this measure will impact on individual IBs should be addressed to the relevant District Lands Office of LandsD. The contact information can be found at`,
-    information_regarding: `Information regarding the zoning of an IB and the “Column 1” always-permitted uses specified in the relevant OZP is available on the website of the Town Planning Board`,
-  },
-  cn: {
-    title: "Relaxation of Waiver Application for Existing Industrial Buildings",
-  },
-};
-
-export const Relaxation: React.FC = () => {
-  const { isPC, language } = useSettings();
-  const page_text =
-    language === Language.EN ? multilingual.en : multilingual.cn;
-  const { title } = page_text;
-
-  const fileList: MediaTemplateWithDialogProps[] = [
-    {
-      title:
-        "Announcement: Relaxation of Waiver Application for Existing Industrial Buildings ",
-      titleCN: ``,
-      date: "1 February 2019",
-      maskIcon: "PDF.png",
-      mediaType: MEDIA_TYPE.PDF,
-      mediaDomain: "devb",
-      mediaLink:
-        "/filemanager/en/Content_3/Relaxation_of_the_Waiver_Application_in_Existing_IB.pdf",
-    },
-    {
-      title:
-        "Press Release: Extending Effective Period of Relaxation of Waiver Application for Existing Industrial Buildings",
-      titleCN: ``,
-      date: "26 January 2024",
-      maskIcon: "PRESS.png",
-      mediaType: MEDIA_TYPE.NEW_PAGE,
-      imagePath: "extending.png",
-      mediaLink:
-        "https://www.info.gov.hk/gia/general/202401/26/P2024012600321.htm",
-    },
-    {
-      title:
-        "Work Focuses of Development Bureau in the Chief Executive’s 2024 Policy Address",
-      titleCN: ``,
-      date: "16 October 2024",
-      maskIcon: "PRESS.png",
-      mediaType: MEDIA_TYPE.NEW_PAGE,
-      imagePath: "Work_Focuses.png",
-      mediaLink:
-        "https://www.devb.gov.hk/en/about_us/policy/2024-policy-address-supplement/policy-address-initiatives/index.html",
-    },
-  ];
-
-  return (
-    <div className="w-full">
-      <div className="mb-[24px]">
-        <SquareTitle title={title} />
-      </div>
-      <p className="mt-[24px] text-body-m text-justify">
-        According to the revitalisation scheme for industrial buildings (“IBs”),
-        one of the measures is to relax the waiver application policy to permit
-        uses for a number of sectors in existing IBs. Testing laboratories may
-        benefit from this measure.
-      </p>
-      <hr className="my-[24px] text-[#E0E0E0]" />
-      <p className="text-heading-l">Background</p>
-      <p className="text-body-m mt-[24px] text-justify">
-        As per the executed land leases, the uses of most IBs are restricted to
-        industrial uses. Under the existing land administration arrangement, if
-        an IB owner wishes to put its premises into always-permitted1
-        non-industrial use under the relevant statutory plan, the owner (or an
-        authorised person) is supposed to submit a temporary waiver application
-        to the Lands Department (LandsD) for temporarily waiving the lease
-        restrictions for the specific premises.
-      </p>
-      <br />
-      <p className="text-body-m text-justify">
-        Note 1: It refers to uses permitted under the land use zoning of the
-        sites concerned on the relevant Outline Zoning Plans as “Column 1” uses.
-      </p>
-      <hr className="my-[24px] text-[#E0E0E0]" />
-      <p className="text-heading-l">
-        Details of the Relaxation of Waiver Application Arrangement
-      </p>
-      <div className="grid grid-cols-2 gap-[8px] my-[24px]">
-        <img
-          className="w-auto h-auto"
-          src={
-            process.env.PUBLIC_URL +
-            "/assets/support/details_of_the_elaxation_1.png"
-          }
-          alt="Professor WONG Wing-tak"
-        />
-        <img
-          className="w-auto h-auto"
-          src={
-            process.env.PUBLIC_URL +
-            "/assets/support/details_of_the_elaxation_2.png"
-          }
-          alt="Professor WONG Wing-tak"
-        />
-      </div>
-      <ul>
-        <li className="!text-body-m">
+      para_1: (
+        <>
           As the Development Bureau announced on 1 February 2019, the Government
           would permit five non-industrial uses in premises within IBs on a
           time-limited basis. The prerequisite is that such uses are always
@@ -145,23 +40,11 @@ export const Relaxation: React.FC = () => {
             further extension to the end of 2027
           </span>
           &nbsp;announced in the 2024 Policy Address.
-        </li>
-        <br />
-        <li className="text-body-m">
-          "Research, Design and Development Centre" is one of the five permitted
-          uses. According to the Definition of Terms published by the Town
-          Planning Board, testing laboratories are covered by “Laboratory,
-          Inspection and Testing Centre” which is subsumed under the use of
-          “Research, Design and Development Centre”. As such, testing
-          laboratories in industrial premises in compliance with the above
-          prerequisites can benefit from the relaxation of waiver application
-          arrangement. In determining whether the use of a premise has involved
-          non-compliant uses outside the scope of the permission, LandsD will
-          seek advice from the Innovation and Technology Commission and the
-          decision of which shall be final.
-        </li>
-        <br />
-        <li className="text-body-m">
+        </>
+      ),
+      para_2: `"Research, Design and Development Centre" is one of the five permitted uses. According to the Definition of Terms published by the Town Planning Board, testing laboratories are covered by “Laboratory, Inspection and Testing Centre” which is subsumed under the use of “Research, Design and Development Centre”. As such, testing laboratories in industrial premises in compliance with the above prerequisites can benefit from the relaxation of waiver application arrangement. In determining whether the use of a premise has involved non-compliant uses outside the scope of the permission, LandsD will seek advice from the Innovation and Technology Commission and the decision of which shall be final.`,
+      para_3: (
+        <>
           For any processed/processing temporary waiver application submitted in
           accordance with&nbsp;
           <a
@@ -173,18 +56,159 @@ export const Relaxation: React.FC = () => {
             Practice Note No. 1/2016
           </a>
           &nbsp;issued by LandsD, no administrative fee will be refunded.
-        </li>
+        </>
+      ),
+      please_refer: `Please refer to the announcement of Development Bureau on 1 February 2019 and the press release of the extension on 26 January 2024, and the 2024 Policy Address for further details:`,
+    },
+    enquiries: `Enquiries`,
+    enquiries_about: `Enquiries about how this measure will impact on individual IBs should be addressed to the relevant District Lands Office of LandsD. The contact information can be found at`,
+    information_regarding: `Information regarding the zoning of an IB and the “Column 1” always-permitted uses specified in the relevant OZP is available on the website of the Town Planning Board`,
+  },
+  cn: {
+    title: "放寬現有工業大廈地契豁免書申請",
+    according_to: `根據活化工業大廈（工廈）計劃，其中一項措施是放寛豁免書申請安排，以准許現有工廈用作若干用途。測試實驗所可受惠於此措施。`,
+    background: "背景",
+    as_per_the: `按已訂立的地契，大部分現有工廈限於用作工業用途。工廈業主如欲將有關處所作所屬法定圖則上的經常准許1之非工業用途，在現行土地行政安排下，該業主（或獲授權人士）本應先向地政總署申請短期豁免書，以豁免該處所的地契限制。`,
+    note: `註1：即該用途為分區計劃大綱圖上該用地所屬地帶「第一欄」的准許用途。`,
+    details_of_relaxation: {
+      title: "放寬地契豁免書申請安排內容",
+      para_1: (
+        <>
+          根據發展局於2019年2月1日的公布，政府在指定期限內，准許將工廈處所用作五類特定的非工業用途，先決條件是該用途必須是所屬法定圖則上的經常准許用途，及不涉及透過直接提供服務或貨品以吸引市民到訪的用途／活動。
+          在此安排下，在指定期限內，現有工廈部分處所的業主可
+          <span className="text-newPrimary !text-highlight-m">無</span>
+          須向地政總署申請短期豁免書及
+          <span className="text-newPrimary !text-highlight-m">無</span>
+          須繳付原本應付的豁免書費用和行政費，而將有關工廈處所用於准許的非工業用途。發展局於2024年1月26日宣布將原定五年的
+          <span className="text-newPrimary !text-highlight-m">指定期限</span>
+          延長至2025年1月31日，隨後於2024年施政報告中宣布將指定期限
+          <span className="text-newPrimary !text-highlight-m">
+            進一步延長至2027年年底
+          </span>
+          。
+        </>
+      ),
+      para_2: `「研究所、設計及發展中心」是上述五類特定非工業用途其中一項。根據城市規劃委員會公布的詞彙釋義，測試實驗所歸屬於「實驗室、檢查測試中心」，而「實驗室、檢查測試中心」歸屬「研究所、設計及發展中心」。因此，如符合上述的先決條件，位於工廈處所的測試實驗所可以受惠於放寬地契豁免書申請安排。在判斷個別處所是否有超出准許範圍的違規用途使用情況時，地政總署會徵詢創新科技署，而所作的決定為最終決定。`,
+
+      para_3: (
+        <>
+          依地政總署
+          <a
+            href="https://www.landsd.gov.hk/en/resources/practice-notes/lao.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-[#00E]"
+          >
+            作業備考1／2016號
+          </a>
+          簽訂的豁免書和申請（包括已獲批准和審批中）的行政費用，不會被退還。
+        </>
+      ),
+      please_refer: `請參閱發展局於2019年2月1日的公布、2024年1月26日關於延長期限的新聞公報、以及2024年施政報告，以了解細節：`,
+    },
+    enquiries: "查詢",
+    enquiries_about: `如對此措施是否適用於個別工廈有任何疑問，請向相關地政總署分區地政處查詢，聯絡資料可瀏覽以下網頁：`,
+    information_regarding: `至於個別工廈在相關分區計劃大綱圖所屬地帶和「第一欄」准許用途的資料，可瀏覽城市規劃委員會網頁。`,
+  },
+};
+
+export const Relaxation: React.FC = () => {
+  const { isPC, language } = useSettings();
+  const page_text =
+    language === Language.EN ? multilingual.en : multilingual.cn;
+  const {
+    title,
+    according_to,
+    background,
+    as_per_the,
+    note,
+    details_of_relaxation,
+    enquiries,
+    enquiries_about,
+    information_regarding,
+  } = page_text;
+
+  const fileList: MediaTemplateWithDialogProps[] = [
+    {
+      title:
+        "Announcement: Relaxation of Waiver Application for Existing Industrial Buildings ",
+      titleCN: `放寬現有工業大廈地契豁免書申請 `,
+      date: "1 February 2019",
+      maskIcon: "PDF.png",
+      mediaType: MEDIA_TYPE.PDF,
+      mediaDomain: "devb",
+      mediaLink:
+        "/filemanager/en/Content_3/Relaxation_of_the_Waiver_Application_in_Existing_IB.pdf",
+    },
+    {
+      title:
+        "Press Release: Extending Effective Period of Relaxation of Waiver Application for Existing Industrial Buildings",
+      titleCN: `新聞公報(2024年1月26日) - 延長期限至2025年1月31日`,
+      date: "26 January 2024",
+      maskIcon: "PRESS.png",
+      mediaType: MEDIA_TYPE.NEW_PAGE,
+      imagePath: "extending.png",
+      mediaLink:
+        "https://www.info.gov.hk/gia/general/202401/26/P2024012600321.htm",
+    },
+    {
+      title:
+        "Work Focuses of Development Bureau in the Chief Executive’s 2024 Policy Address",
+      titleCN: `《行政長官2024年施政報告》有關發展局的工作重點`,
+      date: "16 October 2024",
+      maskIcon: "PRESS.png",
+      mediaType: MEDIA_TYPE.NEW_PAGE,
+      imagePath: "Work_Focuses.png",
+      mediaLink:
+        "https://www.devb.gov.hk/en/about_us/policy/2024-policy-address-supplement/policy-address-initiatives/index.html",
+    },
+  ];
+
+  return (
+    <div className="w-full">
+      <div className="mb-[24px]">
+        <SquareTitle title={title} />
+      </div>
+      <p className="mt-[24px] text-body-m text-justify">{according_to}</p>
+      <hr className="my-[24px] text-[#E0E0E0]" />
+      <p className="text-heading-l">{background}</p>
+      <p className="text-body-m mt-[24px] text-justify">{as_per_the}</p>
+      <br />
+      <p className="text-body-m text-justify">{note}</p>
+      <hr className="my-[24px] text-[#E0E0E0]" />
+      <p className="text-heading-l">{details_of_relaxation.title}</p>
+      <div className="grid grid-cols-2 gap-[8px] my-[24px]">
+        <img
+          className="w-auto h-auto"
+          src={
+            process.env.PUBLIC_URL +
+            "/assets/support/details_of_the_elaxation_1.png"
+          }
+          alt="Professor WONG Wing-tak"
+        />
+        <img
+          className="w-auto h-auto"
+          src={
+            process.env.PUBLIC_URL +
+            "/assets/support/details_of_the_elaxation_2.png"
+          }
+          alt="Professor WONG Wing-tak"
+        />
+      </div>
+      <ul>
+        <li className="!text-body-m">{details_of_relaxation.para_1}</li>
+        <br />
+        <li className="text-body-m">{details_of_relaxation.para_2}</li>
+        <br />
+        <li className="text-body-m">{details_of_relaxation.para_3}</li>
       </ul>
       <br />
-      <p className="text-body-m">
-        Please refer to the announcement of Development Bureau on 1 February
-        2019 and the press release of the extension on 26 January 2024, and the
-        2024 Policy Address for further details:
-      </p>
+      <p className="text-body-m">{details_of_relaxation.please_refer}</p>
       <div className="w-full flex flex-col gap-[24px]">
         {fileList.map((item, index) => {
           const {
             title,
+            titleCN = "",
             imagePath,
             mediaLink,
             mediaType,
@@ -192,6 +216,7 @@ export const Relaxation: React.FC = () => {
             maskIcon,
             date,
           } = item;
+          const displayTitle = language === Language.EN ? title : titleCN;
           return (
             <div
               key={index}
@@ -199,9 +224,9 @@ export const Relaxation: React.FC = () => {
             >
               <MediaTemplateWithDialog
                 title={
-                  title.length > 58 && !isPC
-                    ? title.slice(0, 58) + "..."
-                    : title
+                  displayTitle.length > 58 && !isPC
+                    ? displayTitle.slice(0, 58) + "..."
+                    : displayTitle
                 }
                 imagePath={"support/" + imagePath}
                 mediaLink={mediaLink}
@@ -216,7 +241,7 @@ export const Relaxation: React.FC = () => {
         })}
       </div>
       <hr className="my-[24px] text-[#E0E0E0]" />
-      <p className="text-heading-l">Enquiries</p>
+      <p className="text-heading-l">{enquiries}</p>
       <div className="mt-[24px] border-[1px] border-[#E0E0E0] py-[24px] px-[36px]">
         <div className="flex flex-row gap-[24px] items-center w-full">
           <img
@@ -225,9 +250,7 @@ export const Relaxation: React.FC = () => {
             alt={"alert"}
           />
           <p className="text-body-m flex-grow min-w-0">
-            Enquiries about how this measure will impact on individual IBs
-            should be addressed to the relevant District Lands Office of LandsD.
-            The contact information can be found at
+            {enquiries_about}
             <br />
             <a
               href="https://www.landsd.gov.hk/en/about-us/contact-us.html"
@@ -246,9 +269,7 @@ export const Relaxation: React.FC = () => {
             alt={"alert"}
           />
           <p className="text-body-m flex-grow min-w-0">
-            Information regarding the zoning of an IB and the “Column 1”
-            always-permitted uses specified in the relevant OZP is available on
-            the website of the Town Planning Board
+            {information_regarding}
             <br />(
             <a
               href="https://www.ozp.tpb.gov.hk/"
