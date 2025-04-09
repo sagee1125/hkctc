b962af757fbd3d47ad5cebfd941ad0722809e663
sa1jing <sa1jing.chen@polyu.edu.hk>
2025-04-09 14:24:15 +0800

    fix: amend

--- src/components/Navigator/ExploreBar/ExploreBar.tsx
+++ src/components/Navigator/ExploreBar/ExploreBar.tsx
@@ -127,7 +127,7 @@ export const ExploreBar: React.FC<ExploreBarProps> = ({
           >
             <p className="text-highlight-s">{explore_as as string}</p>
             <svg
-              role="none"
+              role="tab"
               xmlns="https://www.w3.org/2000/svg"
               width="12"
               height="12"
