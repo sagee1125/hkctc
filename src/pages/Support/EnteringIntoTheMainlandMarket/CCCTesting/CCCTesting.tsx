8d1d33b742c16537e48a94b24c659e3a359a8ff8
Qin <719087992@qq.com>
2025-04-19 23:52:16 +0800

    fix(app): 20250419

--- src/pages/Support/EnteringIntoTheMainlandMarket/CCCTesting/CCCTesting.tsx
+++ src/pages/Support/EnteringIntoTheMainlandMarket/CCCTesting/CCCTesting.tsx
@@ -11,145 +11,149 @@ import {
 } from "../../../../components";
 import { navItemEnum } from "../../../../const";
 import { ClausesDirectorySidebar } from "../ClausesDirectorySidebar";
-import { useSettings } from "../../../../context";
-
-const multilingual = {
-  en: {
-    home: "Home",
-    support: "Support - Entering into the Mainland Market",
-    CCC: "China Compulsory Certification (CCC) Testing",
-    CEPA: "CEPA Agreements",
-    since:
-      "Since the signing of the Supplement VII to CEPA, the Mainland has been gradually opening up its CCC market to Hong Kong testing and certification bodies. Testing organisations in Hong Kong are now allowed to co-operate with designated Mainland certification bodies to undertake testing of all products that require CCC which are processed or manufactured in any place (including places outside China).",
-    list: [
-      "Electric cables and wiring",
-      "Electrical switches, protective devices and connection devices",
-      "Low voltage electrical equipment",
-      "Small power motors",
-      "Electric tools",
-      "Electric welding machines",
-      "Household and similar electrical appliances",
-      "Electronic product and safety accessory",
-      "Lighting apparatus",
-      "Motor vehicles and safety accessories",
-      "Agricultural machinery",
-      "Fire service equipment",
-      "Construction materials",
-      "Children’s products",
-      "Explosion-proof electrical products",
-      "Household gas appliances",
-    ],
-    any_testing: (
-      <>
-        Any testing organisations engaging in testing business for products
-        under the CCC System have to be accredited by Hong Kong Accreditation
-        Service (HKAS) to be capable of performing testing for the relevant
-        products. Procedures for testing organisations in applying for relevant
-        accreditation from HKAS could be viewed at&nbsp;
-        <a
-          aria-label="HKAS's website  – opens in a new tab"
-          href="https://www.itc.gov.hk/en/quality/hkas/conformity_assessment_bodies/index.html"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline text-[#00E]"
-        >
-          HKAS's website
-        </a>
-        .
-      </>
-    ),
-    list_of: "List of existing products requiring CCC",
-    enquiry: (
-      <>
-        For enquiry on accreditation for CCC testing, please contact:
-        <br />
-        Hong Kong Accreditation Service
-        <br />
-        Telephone: (852) 2829 4826
-        <br />
-        Fax: (852) 2824 1302
-        <br />
-        Email:&nbsp;
-        <a
-          aria-label="mail to wmleung@itc.gov.hk"
-          className="underline text-[#00E]"
-          tabIndex={0}
-          href="mailto:wmleung@itc.gov.hk"
-        >
-          wmleung@itc.gov.hk
-        </a>
-      </>
-    ),
-    bread_title:
-      "China Compulsory Certification (CCC) Testing or International Multilateral Systems on Mutual Recognition",
-  },
-  cn: {
-    home: "主頁",
-    support: "支援 - 進入內地市場",
-    CCC: "中國強制性產品認證（CCC）檢測",
-    CEPA: "CEPA協議",
-    list_of: "現行需要CCC認證的產品包括：",
-    since:
-      "自簽署《CEPA補充協議七》起，內地逐步對香港檢測和認證機構開放其CCC市場。現時香港檢測機構可與內地指定認證機構合作，為在任何地區（包括中國以外）加工或生產並需CCC認證的產品進行檢測工作。",
-    list: [
-      `電線電纜`,
-      `電路開關及保護或連接用電器裝置`,
-      `低壓電器`,
-      `小功率電動機`,
-      `電動工具`,
-      `電焊機`,
-      `家用和類似用途設備`,
-      `電子產品及安全附件`,
-      `照明電器`,
-      `車輛及安全附件`,
-      `農機產品`,
-      `消防產品`,
-      `建材產品`,
-      `兒童用品`,
-      `防爆電氣`,
-      `家用燃氣器具`,
-    ],
-    any_testing: (
-      <>
-        任何擬從事CCC檢測工作的檢測實驗所須獲得香港認可處認可，以確認該實驗所具備相關檢測能力。有關申請香港認可處認可的程序，請見香港認可處的&nbsp;
-        <a
-          aria-label="香港認可處"
-          href="https://www.itc.gov.hk/en/quality/hkas/conformity_assessment_bodies/index.html"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline text-[#00E]"
-        >
-          網頁
-        </a>
-        。
-      </>
-    ),
-    enquiry: (
-      <>
-        如有任何有關CCC檢測之認可的查詢，請聯絡：
-        <br />
-        香港認可處
-        <br />
-        電話：（852）2829 4846
-        <br />
-        傳真：（852）2824 1302
-        <br />
-        電子郵件：
-        <a
-          aria-label="mail to wmleung@itc.gov.hk"
-          className="underline text-[#00E]"
-          tabIndex={0}
-          href="mailto:wmleung@itc.gov.hk"
-        >
-          wmleung@itc.gov.hk
-        </a>
-      </>
-    ),
-  },
-};
+import { Language, useSettings } from "../../../../context";
 
 export const CCCTesting: React.FC = () => {
-  const { isPC, getPageText } = useSettings();
+  const { isPC, language, getPageText } = useSettings();
+  const multilingual = {
+    en: {
+      home: "Home",
+      support: "Support - Entering into the Mainland Market",
+      CCC: "China Compulsory Certification (CCC) Testing",
+      CEPA: "CEPA Agreements",
+      since:
+        "Since the signing of the Supplement VII to CEPA, the Mainland has been gradually opening up its CCC market to Hong Kong testing and certification bodies. Testing organisations in Hong Kong are now allowed to co-operate with designated Mainland certification bodies to undertake testing of all products that require CCC which are processed or manufactured in any place (including places outside China).",
+      list: [
+        "Electric cables and wiring",
+        "Electrical switches, protective devices and connection devices",
+        "Low voltage electrical equipment",
+        "Small power motors",
+        "Electric tools",
+        "Electric welding machines",
+        "Household and similar electrical appliances",
+        "Electronic product and safety accessory",
+        "Lighting apparatus",
+        "Motor vehicles and safety accessories",
+        "Agricultural machinery",
+        "Fire service equipment",
+        "Construction materials",
+        "Children’s products",
+        "Explosion-proof electrical products",
+        "Household gas appliances",
+      ],
+      any_testing: (
+        <>
+          Any testing organisations engaging in testing business for products
+          under the CCC System have to be accredited by Hong Kong Accreditation
+          Service (HKAS) to be capable of performing testing for the relevant
+          products. Procedures for testing organisations in applying for
+          relevant accreditation from HKAS could be viewed at&nbsp;
+          <a
+            aria-label="HKAS's website  – opens in a new tab"
+            href="https://www.itc.gov.hk/en/quality/hkas/conformity_assessment_bodies/index.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-[#00E]"
+          >
+            HKAS's website
+          </a>
+          .
+        </>
+      ),
+      list_of: "List of existing products requiring CCC",
+      enquiry: (
+        <>
+          For enquiry on accreditation for CCC testing, please contact:
+          <br />
+          Hong Kong Accreditation Service
+          <br />
+          Telephone: (852) 2829 4826
+          <br />
+          Fax: (852) 2824 1302
+          <br />
+          Email:&nbsp;
+          <a
+            aria-label="mail to wmleung@itc.gov.hk"
+            className="underline text-[#00E]"
+            tabIndex={0}
+            href="mailto:wmleung@itc.gov.hk"
+          >
+            wmleung@itc.gov.hk
+          </a>
+        </>
+      ),
+      bread_title:
+        "China Compulsory Certification (CCC) Testing or International Multilateral Systems on Mutual Recognition",
+    },
+    cn: {
+      home: "主頁",
+      support: "支援 - 進入內地市場",
+      CCC: "中國強制性產品認證（CCC）檢測",
+      CEPA: "CEPA協議",
+      list_of: "現行需要CCC認證的產品包括：",
+      since:
+        "自簽署《CEPA補充協議七》起，內地逐步對香港檢測和認證機構開放其CCC市場。現時香港檢測機構可與內地指定認證機構合作，為在任何地區（包括中國以外）加工或生產並需CCC認證的產品進行檢測工作。",
+      list: [
+        `電線電纜`,
+        `電路開關及保護或連接用電器裝置`,
+        `低壓電器`,
+        `小功率電動機`,
+        `電動工具`,
+        `電焊機`,
+        `家用和類似用途設備`,
+        `電子產品及安全附件`,
+        `照明電器`,
+        `車輛及安全附件`,
+        `農機產品`,
+        `消防產品`,
+        `建材產品`,
+        `兒童用品`,
+        `防爆電氣`,
+        `家用燃氣器具`,
+      ],
+      any_testing: (
+        <>
+          任何擬從事CCC檢測工作的檢測實驗所須獲得香港認可處認可，以確認該實驗所具備相關檢測能力。有關申請香港認可處認可的程序，請見香港認可處的
+          <a
+            aria-label="香港認可處"
+            href={
+              language === Language.ZH_CN
+                ? "https://www.itc.gov.hk/ch/quality/hkas/accreditation/ccc.html"
+                : "https://www.itc.gov.hk/gb/quality/hkas/accreditation/ccc.html"
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-[#00E]"
+          >
+            網頁
+          </a>
+          。
+        </>
+      ),
+      enquiry: (
+        <>
+          如有任何有關CCC檢測之認可的查詢，請聯絡：
+          <br />
+          香港認可處
+          <br />
+          電話：（852）2829 4846
+          <br />
+          傳真：（852）2824 1302
+          <br />
+          電子郵件：
+          <a
+            aria-label="mail to wmleung@itc.gov.hk"
+            className="underline text-[#00E]"
+            tabIndex={0}
+            href="mailto:wmleung@itc.gov.hk"
+          >
+            wmleung@itc.gov.hk
+          </a>
+        </>
+      ),
+    },
+  };
+
   const page_text = getPageText(multilingual);
   const {
     home,
